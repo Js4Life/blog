ebcdb26e808ca00e33a6131c7fcbb30b3e579a90
Netanel Basal <netanel7799@gmail.com>
2019-06-24 18:13:38 +0300

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Netanel Basal',
   githubUrl: 'https://github.com/NetanelBasal/blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'netbasal',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: '',
